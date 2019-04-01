927ef590e28bb8f9c7049fa9b882c6d13bc7d66d
QiminCao <sevenscao@gmail.com>
2019-03-31 22:04:21 -0700

    Added search box

--- imports/ui/Search.jsx
+++ imports/ui/Search.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import SearchBox from "./Search.jsx";
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Welcome to Meteor!</h1>
-				<SearchBox />
+				<input type="search"/>
 			</div>
 		);
 	}
