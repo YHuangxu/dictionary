2cf9691ad038dfdbdc88cd075dffea7739b3bcd6
QiminCao <sevenscao@gmail.com>
2019-04-02 22:18:11 -0700

    Finished api call

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,40 @@
 import React from "react";
-import SearchBar from "./Search.jsx";
+import SearchBar from "./Search";
+import WordsList from "./WordsList";
+import wordAPI from "../api/wordAPI";
+
 import { Container } from "semantic-ui-react";
 
-
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			words: []
+		};
+	}
+
+	onSearchSubmit(word) {
+		wordAPI
+			.get(`words/${word}`)
+			.then(res => this.setState({ words: res.data.results }));
+	}
+
+	// onWordSubmit = async word => {
+	// 	const response = await words.get(`words/${word}`);
+	// 	let wordslimit = [];
+	// 	for (let i = 0; i < Math.min(response.data.results.length, 10); i++) {
+	// 		wordslimit.push(response.data.results[i]);
+	// 	}
+	// 	this.setState({ words: wordslimit });
+	// };
+
 	render() {
 		return (
 			<Container>
 				<h1>Welcome to Meteor!</h1>
-				<SearchBar />
+				<SearchBar onSubmit={this.onSearchSubmit.bind(this)} />
+
+				<WordsList words={this.state.words} />
 			</Container>
 		);
 	}
---
+++ imports/ui/WordsList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import WordItem from "./WordItem";
+import { Grid, Container } from "semantic-ui-react";
+
+export default class WordList extends React.Component {
+	renderWordsList() {
+		return this.props.words.map((word, index) => (
+			<Grid.Column width={4} key={index}>
+				<WordItem word={word} />
+			</Grid.Column>
+		));
+	}
+
+	render() {
+		return (
+			<Container>
+				<Grid centered>{this.renderWordsList()}</Grid>
+			</Container>
+		);
+	}
+}
+
+WordList.propTypes = {
+	words: PropTypes.arrayOf(PropTypes.object).isRequired
+};
