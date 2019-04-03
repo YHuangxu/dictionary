cb4ca475b1eaf7b36cdb3dda3a3df5dba42dfe6e
QiminCao <sevenscao@gmail.com>
2019-04-03 02:11:30 -0700

    Remove wordAPI

--- imports/api/wordAPI.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import KEY from "./wordsAPI_config.js";
-
-export default axios.create({
-	baseURL: "https://wordsapiv1.p.rapidapi.com/",
-	headers: {
-		"X-RapidAPI-Key": KEY
-	}
-});
