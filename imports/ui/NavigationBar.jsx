3e6511fe34a67a0aa6d4d5bccadb4f4e3ecb8201
freddy <zhonglyu173@gmail.com>
2019-04-03 16:28:19 -0700

    modifying login and dropdown

--- imports/ui/NavigationBar.jsx
+++ imports/ui/NavigationBar.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { Button, Dropdown, Menu } from "semantic-ui-react";
 import { Meteor } from "meteor/meteor";
-import Lonin from "./Login.jsx";
+import Login from "./Login.jsx";
 import { Accounts } from "meteor/accounts-base";
+import { withTracker } from "meteor/react-meteor-data";
+import PropTypes from "prop-types";
 
-export default class NavigationBar extends Component {
+class NavigationBar extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			isLoggedin: false
+		};
 		this.loginRef = React.createRef();
 	}
 
@@ -18,6 +23,39 @@ export default class NavigationBar extends Component {
 		this.loginRef.current.openModal();
 	}
 
+	displayLogin() {
+		return (
+			<Menu.Item>
+				<Button onClick={this.handleClick.bind(this)}>Log In</Button>
+				<Login ref={this.loginRef} />
+			</Menu.Item>
+		);
+	}
+
+	displayDropdown() {
+		return (
+			<Menu.Item>
+				<Dropdown
+					text={
+						Meteor.user()
+							? "User: " + Meteor.user().username
+							: "Please log in"
+					}
+				>
+					<Dropdown.Menu>
+						<Dropdown.Item icon="folder" text="My Lists" />
+						<Dropdown.Divider />
+						<Dropdown.Item
+							icon="log out"
+							text="Log Out"
+							onClick={this.onLogout.bind(this)}
+						/>
+					</Dropdown.Menu>
+				</Dropdown>
+			</Menu.Item>
+		);
+	}
+
 	render() {
 		return (
 			<Menu secondary>
@@ -25,31 +63,22 @@ export default class NavigationBar extends Component {
 				<Menu.Item name="type.js" />
 
 				<Menu.Menu position="right">
-					<Menu.Item>
-						<Button onClick={this.handleClick.bind(this)}>Log In</Button>
-						<Lonin ref={this.loginRef}/>
-					</Menu.Item>
-					<Menu.Item>
-						<Dropdown
-							text={
-								Meteor.user()
-									? "User: " + Meteor.user().username
-									: "Please log in"
-							}
-						>
-							<Dropdown.Menu>
-								<Dropdown.Item icon="folder" text="My Lists" />
-								<Dropdown.Divider />
-								<Dropdown.Item
-									icon="log out"
-									text="Log Out"
-									onClick={this.onLogout.bind(this)}
-								/>
-							</Dropdown.Menu>
-						</Dropdown>
-					</Menu.Item>
+					{Meteor.user() ? "" : this.displayLogin()}
+					{this.props.user ? this.displayDropdown() : ""}
 				</Menu.Menu>
 			</Menu>
 		);
 	}
 }
+
+// Below are two ways to get the value of Meteor.user()
+// 1.
+NavigationBar.propTypes = {
+	user: PropTypes.bool.isRequired
+};
+//2.
+export default withTracker(() => {
+	return {
+		user: !!Meteor.user()
+	};
+})(NavigationBar);
