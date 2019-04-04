0e212b78bc5f5aa36f6ad264c3244f277697439e
QiminCao <sevenscao@gmail.com>
2019-04-04 11:53:57 -0700

    Integrated api call into a meteor method

--- imports/api/wordsAPI.js
+++ imports/api/wordsAPI.js
@@ -1,9 +1,22 @@
+import { Meteor } from "meteor/meteor";
 import axios from "axios";
 import apiKey from "./wordsAPI_config.js";
 
-export default axios.create({
-	baseURL: "https://wordsapiv1.p.rapidapi.com/",
-	headers: {
-		"X-RapidAPI-Key": apiKey.KEY
-	}
-});
+if (Meteor.isServer) {
+	Meteor.methods({
+		"getData"(param) {
+			const wordsAPI = axios.create({
+				baseURL: "https://wordsapiv1.p.rapidapi.com/",
+				headers: {
+					"X-RapidAPI-Key": apiKey.KEY
+				}
+			});
+
+			return wordsAPI
+				.get(param)
+				.then(res => {
+					return res.data;
+				});
+		}
+	});
+}
--- server/main.js
+++ server/main.js
@@ -1,3 +1,4 @@
 import "../imports/startup/simpl-schema-config.js";
 import "../imports/api/users.js";
 import "../imports/api/lists.js";
+import "../imports/api/wordsAPI.js";
