83f96968d6f813c11956ae55d31669a7443b3f5b
QiminCao <sevenscao@gmail.com>
2019-04-05 00:58:40 -0700

    Added rate limiter

--- server/main.js
+++ server/main.js
@@ -2,13 +2,25 @@ import "../imports/startup/simpl-schema-config.js";
 import "../imports/api/users.js";
 import "../imports/api/lists.js";
 import "../imports/api/wordsAPI.js";
+import { Meteor } from "meteor/meteor";
 import { DDPRateLimiter } from "meteor/ddp-rate-limiter";
 
-// Define a rule that matches login attempts by non-admin users.
-const apiCallRule = {
-	type: "method",
-	name: "apiCall"
-};
 
-// Add the rule, allowing up to 5 messages every 1000 milliseconds.
-DDPRateLimiter.addRule(apiCallRule, 5, 1000);
+// Get list of all method names on Lists
+const LISTS_METHODS = [
+	"defaultList.insert",
+	"defaultList.remove",
+	"getData"
+];
+
+// Only allow 5 list operations per connection per second
+if (Meteor.isServer) {
+	DDPRateLimiter.addRule({
+		name(name) {
+			return LISTS_METHODS.includes(name);
+		},
+
+		// Rate limit per connection ID
+		connectionId() { return true; }
+	}, 5, 1000);
+}
