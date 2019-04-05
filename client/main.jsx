1f122c51cdf96fc4a1c11f31c2bb17df3f748f55
freddy <zhonglyu173@gmail.com>
2019-04-04 17:13:30 -0700

    deleting dev for css

--- client/main.jsx
+++ client/main.jsx
@@ -1,7 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import { renderRoutes } from "../imports/routers/Routers.js"; 
-import "semantic-ui-css/semantic.min.css";
 
 Meteor.startup(() => {
 	render(renderRoutes(), document.getElementById("react-target"));
