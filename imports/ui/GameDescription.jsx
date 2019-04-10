e852aed6566767e9e3537b415e0c08642c9098b9
freddy <zhonglyu173@gmail.com>
2019-04-10 10:56:25 -0700

    changing review to challenge

--- imports/ui/GameDescription.jsx
+++ imports/ui/GameDescription.jsx
@@ -5,20 +5,25 @@ export default class GameDescription extends React.Component {
 	render() {
 		return (
 			<Message>
-				<Message.Header>How to Play</Message.Header>
+				<Message.Header>How to Chanllenge Yourself</Message.Header>
 				<Message.List>
 					<Message.Item>
-						Click play button to start a review game
+						Click play button to start challenging yourself
 					</Message.Item>
 					<Message.Item>
 						You will be randomly assigned an opponent
 					</Message.Item>
 					<Message.Item>
-						Each player has 10 words, and game is over the moment whoever finishes reviewing
+						You will be given 10 words picked randomly from all
+						searching records
 					</Message.Item>
 					<Message.Item>
-						Every word is worth 10 points. The winner will be awarded
-						100 points
+						Game is over the moment whoever finishes to answer the
+						questions
+					</Message.Item>
+					<Message.Item>
+						Every word is worth 10 points. The winner will be
+						awarded 100 points
 					</Message.Item>
 				</Message.List>
 			</Message>
