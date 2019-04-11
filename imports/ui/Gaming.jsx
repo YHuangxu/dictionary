890695b645dd88c3d19735eb9b85cebe1b944171
freddy <zhonglyu173@gmail.com>
2019-04-10 18:12:45 -0700

    subscribe games collection

--- imports/ui/Gaming.jsx
+++ imports/ui/Gaming.jsx
@@ -6,23 +6,50 @@ import { Session } from "meteor/session";
 import NavigationBar from "./NavigationBar.jsx";
 
 import { withTracker } from "meteor/react-meteor-data";
-import { Container } from "semantic-ui-react";
+import { Container, Grid, Card, Icon, Button } from "semantic-ui-react";
 
 import { Games } from "../lib/games.js";
-
+import { allSearchedWords } from "../api/allSearchedWords.js";
 class Gaming extends React.Component {
+
+	renderCards() {
+		return this.props.wordsDataSet.map(word => (
+			<Card>
+				<Card.Content header="Word Definition" />
+				<Card.Content> {} </Card.Content>
+				<Card.Content extra>
+					<Button>1</Button>
+					<Button>1</Button>
+					<Button>1</Button>
+				</Card.Content>
+			</Card>
+		));
+	}
+
 	render() {
 		return (
 			<Container>
 				<NavigationBar />
 				{this.props.gameEnded ? "Game End" : "Game playing"}
+
+				<Grid celled centered textAlign="center">
+					<Grid.Row>
+						<Grid.Column width={4} />
+
+						<Grid.Column width={8}>
+							{this.renderCards()}
+						</Grid.Column>
+						<Grid.Column width={4} />
+					</Grid.Row>
+				</Grid>
 			</Container>
 		);
 	}
 }
 
 Gaming.propTypes = {
-	gameEnded: PropTypes.bool.isRequired
+	gameEnded: PropTypes.bool.isRequired,
+	wordsDataSet: 
 };
 
 function gameEnded() {
@@ -44,9 +71,16 @@ function gameEnded() {
 
 export default withTracker(() => {
 	const handle = Meteor.subscribe("games");
-
+	Meteor.subscribe("allSearchedWords");
+	let uniqueWords = new Set(allSearchedWords.find({}).fetch());
+	let words = [];
+	for (var i = 0; i < 10; i++) {
+		words.push(uniqueWords.get(i));
+	}
 	return {
 		gameEnded: gameEnded(),
-		ready: handle.ready()
+		ready: handle.ready(),
+
+		wordsDataSet: uniqueWords
 	};
 })(Gaming);
