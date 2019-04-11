d642d984a94671999ac32db0de92d2ebd428b7d8
freddy <zhonglyu173@gmail.com>
2019-04-11 02:31:09 -0700

    Track user log out

--- server/main.js
+++ server/main.js
@@ -3,9 +3,10 @@ import "../imports/api/users.js";
 import "../imports/api/lists.js";
 import "../imports/api/allSearchedWords.js";
 import "../imports/api/wordsAPI.js";
+import { UserStatus } from "meteor/mizzao:user-status";
 
-import "../imports/lib/games.js";
-import "../imports/lib/gameLogic.js";
+import { Games } from "../imports/lib/games.js";
+import { gameLogic } from "../imports/lib/gameLogic.js";
 
 import { Meteor } from "meteor/meteor";
 import { DDPRateLimiter } from "meteor/ddp-rate-limiter";
@@ -14,20 +15,49 @@ import { WebApp } from "meteor/webapp";
 WebApp.addHtmlAttributeHook(() => ({ lang: "en" }));
 
 // Get list of all method names on Lists
-const LISTS_METHODS = [
-	"defaultList.insert",
-	"defaultList.remove",
-	"getData"
-];
+const LISTS_METHODS = ["defaultList.insert", "defaultList.remove", "getData"];
 
 // Only allow 5 list operations per connection per second
 if (Meteor.isServer) {
-	DDPRateLimiter.addRule({
-		name(name) {
-			return LISTS_METHODS.includes(name);
-		},
+	DDPRateLimiter.addRule(
+		{
+			name(name) {
+				return LISTS_METHODS.includes(name);
+			},
 
-		// Rate limit per connection ID
-		connectionId() { return true; }
-	}, 5, 1000);
+			// Rate limit per connection ID
+			connectionId() {
+				return true;
+			}
+		},
+		5,
+		1000
+	);
 }
+
+UserStatus.events.on("connectionLogout", fields => {
+	const game = Games.findOne({
+		$or: [{ player1: fields.userId }, { player2: fields.userId }]
+	});
+
+	if (game != undefined) {
+		if (game.gameStatus !== "waiting" && game.gameStatus !== "gameover") {
+			if (game.player1 === fields.userId) {
+				gameLogic.setGameResult(game._id, game.player2);
+				gameLogic.removePlayer(game._id, "player1");
+			} else if (game.player2 === fields.userId) {
+				gameLogic.setGameResult(game._id, game.player1);
+				gameLogic.removePlayer(game._id, "player2");
+			}
+		} else {
+			if (game.player1 === "" || game.player2 === "") {
+				gameLogic.removeGame(game._id);
+			} else {
+				if (game.player1 === fields.userId)
+					gameLogic.removePlayer(game._id, "player1");
+				else if (game.player2 === fields.userId)
+					gameLogic.removePlayer(game._id, "player2");
+			}
+		}
+	}
+});
