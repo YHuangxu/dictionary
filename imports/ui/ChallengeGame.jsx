e80bdc7746d8e9c3bdc921c692b9a1a555d787a0
freddy <zhonglyu173@gmail.com>
2019-04-11 21:22:32 -0700

    refacotring go to challenge button

--- imports/ui/ChallengeGame.jsx
+++ imports/ui/ChallengeGame.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+
+import { Meteor } from "meteor/meteor";
+import { Link } from "react-router-dom";
+import { withTracker } from "meteor/react-meteor-data";
 
 import NavigationBar from "./NavigationBar.jsx";
 import GameDescription from "./GameDescription";
 
-import { Button } from "semantic-ui-react";
+import { DefaultList } from "../api/lists";
 
-export default class ChallengeGame extends React.Component {
-	handleChallengeClick() {
-		this.props.history.push("/challenge/playing");	
+import { Container, Button, Modal, Header, Icon } from "semantic-ui-react";
+
+class ChallengeGame extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			modalOpen: false
+		};
 	}
+
+	handleChallengeClick() {
+		if (this.props.myWords.length < 10) {
+			this.setState({
+				modalOpen: true
+			});
+		} else {
+			this.props.history.push("/challenge/playing");
+		}
+	}
+
+	// modalCheck() {
+
+	// }
 
 	render() {
 		return (
@@ -25,15 +46,66 @@ export default class ChallengeGame extends React.Component {
 				<Link to="/glossary">Back my Word List</Link>
 				<br />
 
-				<Button positive onClick={() => this.handleChallengeClick()}>
-					Go To Challenge
-				</Button>
-
+				<Modal
+					trigger={
+						<Button
+							positive
+							onClick={() => this.handleChallengeClick()}
+						>
+							Go To Challenge
+						</Button>
+					}
+					open={this.state.modalOpen}
+				>
+					<Header
+						icon="info circle"
+						content="Word Hard, Play Harder"
+					/>
+					<Modal.Content>
+						<p>
+							Your list has less than 10 words. Do you want to
+							review by you own?
+						</p>
+					</Modal.Content>
+					<Modal.Actions>
+						<Link to="/glossary">
+							<Button
+								color="green"
+								onClick={() =>
+									this.setState({ modalOpen: false })
+								}
+							>
+								<Icon name="checkmark" /> Yes
+							</Button>
+						</Link>
+						<Link to="/">
+							<Button
+								color="red"
+								onClick={() =>
+									this.setState({ modalOpen: false })
+								}
+							>
+								<Icon name="remove" /> No
+							</Button>
+						</Link>
+					</Modal.Actions>
+				</Modal>
 			</Container>
 		);
 	}
 }
 
 ChallengeGame.propTypes = {
-	history: PropTypes.object.isRequired
+	history: PropTypes.object.isRequired,
+	myWords: PropTypes.arrayOf(PropTypes.object).isRequired
 };
+
+export default withTracker(() => {
+	Meteor.subscribe("defaultList");
+
+	return {
+		myWords: DefaultList.find({
+			userId: Meteor.userId()
+		}).fetch()
+	};
+})(ChallengeGame);
